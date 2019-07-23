c37c7a38efb0f12fe7d1c8bbd7ca282144cdec90
w3cj <cj@null.computer>
2019-07-22 19:37:38 -0600

    :tada: add the demo

--- image-search-example/src/App.vue
+++ image-search-example/src/App.vue
@@ -1,11 +1,47 @@
 <template>
   <div id="app">
-    <h1>Hello World!</h1>
+    <form @submit.prevent="searchGifs">
+      <label>Search</label>
+      <input v-model="search">
+    </form>
+    <div v-if="loading">
+      <img alt="loading" src="https://media2.giphy.com/media/l3nWhI38IWDofyDrW/giphy.gif?cid=790b76115d055ab7424f75514dcb4d7a&rid=giphy.gif" />
+    </div>
+    <div v-if="!loading" class="images">
+      <img v-for="gif in gifs" :src="gif" :key="gif" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { value } from 'vue-function-api';
+
+const API_URL = 'https://api.giphy.com/v1/gifs/search?api_key=JnxTmEGKXjZeUKBzRjTQoMDg8OX8pS5U&rating=pg&q=';
+
+export default {
+  setup() {
+    const search = value('');
+    const loading = value(false);
+    const gifs = value([]);
+
+    const searchGifs = async () => {
+      loading.value = true;
+      const response = await fetch(API_URL + search.value);
+      const json = await response.json();
+      gifs.value = json.data.map(gif => gif.images.fixed_height.url);
+      setTimeout(() => {
+        loading.value = false;
+      }, 1500);
+    };
+
+    return {
+      search,
+      loading,
+      searchGifs,
+      gifs,
+    };
+  },
+};
 </script>
 
 <style>
--- image-search-example/src/main.js
+++ image-search-example/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import { plugin } from 'vue-function-api';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(plugin);
 
 new Vue({
   render: h => h(App),
